08cdd817a58f538c917601c8fe4f4c4813c1f184
Santiago Toledano <stoledano@autentia.com>
2016-09-26 20:26:23 +0200

    Modifies compare testcase

--- src/test/java/mf/carva/MathOperationTest.java
+++ src/test/java/mf/carva/MathOperationTest.java
@@ -4,24 +4,23 @@ import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsEqual.equalTo;
 
 public class MathOperationTest {
 
     @Test
     public void
-    shouldCompareTwoInteger(){
-        
+    shouldCompareTwoInteger() {
+
         // ARRANGE
         int a = 10;
         int b = 20;
 
         // ACT
         MathOperation sut = new MathOperation();
-        sut.compare(a,b);
-        
+        boolean equal = sut.compare(a, b);
+
         // ASSERT
-        assertThat(a,is(equalTo(b)));
+        assertThat(equal, is(Boolean.TRUE));
     }
 
 }
